005c6b1e38bb8ed52ae4be94937668c3ce2a9d2c
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-02 20:19:56 +0100

    refactor: rename tailwind theme font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from "tailwindcss/plugin";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        grotesk: ['"MonumentGrotesk Mono"', "sans-serif"],
+        body: ['"MonumentGrotesk Mono"', 'sans-serif'],
       },
     },
   },
